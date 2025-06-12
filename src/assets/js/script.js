951c1d9f520430bdf83b41a7828f311869fc1767
doctorscript <kelsondesousa99@gmail.com>
2025-06-13 00:25:36 +0100

    Layout mobile pronto

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,30 +1,11 @@
-var hora = new Date().getHours()
-const search = document.querySelector('#pesquisar')
+const open = document.querySelector(".open")
+const close = document.querySelector(".close")
+const mobile_menu = document.querySelector(".mobile-menu")
 
-if (hora < 12) {
-    document.body.style.backgroundImage = "url('./src/assets/images/dia.jpg')"
-} else if (hora < 18) {
-    document.body.style.backgroundImage = "url('./src/assets/images/tarde.jpg')"
-} else {
-    document.body.style.backgroundImage = "url('./src/assets/images/noite.jpg')"
-}
+open.addEventListener('click', () => {
+    mobile_menu.classList.remove("hidden")
+})
 
-const buscarCidade = async () => {
-    const promiseCidade = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&lang=pt_br&appid=353ba8d3c9de3fac8fe2158b1f438bb7`)
-    const result = await promiseCidade.json()
-
-    console.log(result)
-    document.querySelector('.pais').innerHTML = result['sys']['country']
-    document.querySelector('.cidade').innerHTML = result['name']
-    document.querySelector('.temperatura').innerHTML = Math.round(result['main']['temp'])
-    document.querySelector('.clima-descricao').innerHTML = result['weather']['0']['description']
-    document.querySelector('.icone-clima').innerHTML = result['weather']['0']['icon']
-}
-
-search.addEventListener('keydown', async (event) => {
-    if (event.key === 'Enter') {
-        if (search.value.lenght !== 0) {
-            buscarCidade()
-        }
-    }
+close.addEventListener('click', () => {
+    mobile_menu.classList.add("hidden")
 })
